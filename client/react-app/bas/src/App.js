233a4398ad6e528c0d181b834f6e00e456ebd9b4
saifattq <72181778+saifahmad02@users.noreply.github.com>
2022-11-30 00:13:17 -0500

    Fixed Routing and collapsable menu

--- client/react-app/bas/src/App.js
+++ client/react-app/bas/src/App.js
@@ -1,13 +1,17 @@
 import {Route, Routes} from 'react-router-dom';
 import "./index.css"
-import Home from './components/Home';
 import Login from './components/Login';
+import Playlists from './components/Playlists';
+import Sidebar from './components/Sidebar';
+import Discover from './components/Discover';
 function App() {
   return (
-    <div className=''>
+    <div className='relative flex h-screen'>
+      <Sidebar />
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<Discover />} />
+        <Route path="/playlists" element={<Playlists />} />
       </Routes>
     </div>
   );
--- client/react-app/bas/src/components/Discover.js
+++ client/react-app/bas/src/components/Discover.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import Searchbar from './Searchbar'
 
 const Discover = () => {
   return (
     <div className="flex flex-col m-8">
         <div className='w-full flex flex-col justify-between items-center mb-5'>
-            <h2 className='font-bold text-white text-5xl text-left'>Discover</h2>
+            <h2 className='font-semibold text-white text-5xl text-left'>Discover</h2>
         </div>
+        <Searchbar />
         <div className='flex flex-wrap justify-center gap-8 text-white'>
         </div>
     </div>
