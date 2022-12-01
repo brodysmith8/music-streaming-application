e0abaefaf6095f1b8b761d246198afe6d8c5f30e
Brody Smith <bsmit272@uwo.ca>
2022-12-01 10:14:48 -0500

    Revert to old state
    That idea would require extensive refactoring of DB connection

--- server/general.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-exports.api = function (req, res) {
-    res.send('Please add /tracks, /albums, /artist, or /genre followed by a resource reference after your request.');
-    return;
-}
--- server/helpers.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-exports.minutesToSeconds = function (inputText) {
-    const minutes = parseInt(inputText.split(':')[0]);
-    const seconds = parseInt(inputText.split(':')[1]);
-    return minutes * 60 + seconds;
-}
