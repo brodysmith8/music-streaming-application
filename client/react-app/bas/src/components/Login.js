770699afaeda9266fbeb8441100560a417f9a9d8
aelzein2 <aelzein2@uwo.ca>
2022-12-06 20:43:03 -0500

    Addition of Application name and details
    Application name and about details are displayed on the start page along side a login button

--- client/react-app/bas/src/components/Login.js
+++ client/react-app/bas/src/components/Login.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { RiGoogleFill } from 'react-icons/ri';
 import jwtDecode from 'jwt-decode';
+import { TbDetails } from 'react-icons/tb';
 
 
 const Login = () => {
@@ -33,34 +34,58 @@ const Login = () => {
         navigate(path);
     }
 
-    
 
   return (
-    <div className='loginBG flex h-screen'>
-        <div className="w-full max-w-xs m-auto">
-            <form className="bg-red-200 bg-opacity-20 backdrop-blur-sm rounded drop-shadow-lg px-8 pt-6 pb-8 mb-4">
-                <div className="mb-4">
-                    <label className="block text-white text-sm font-bold mb-2" htmlFor="username">
-                        Username
-                    </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
+     
+   
+        <div className='loginBG flex h-screen'>
+            
+             <div className="w-full max-w-xs m-auto">
+                <div className = 'applicationName'>
+                    <div className = 'text-5xl font-bold text-white text-center absolute inset-y-20 inset-x-50'>
+                        BAS MUSIC APP
                     </div>
-                <div className="mb-6">
-                    <label className="block text-white text-sm font-bold mb-2" htmlFor="password">
-                        Password
-                    </label>
-                    <input className="shadow appearance-none rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
-                </div>
-                <div className="flex items-center justify-between">
-                    <button className="bg-black hover:bg-red-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={routeChange}>
-                        Sign In
-                    </button>
-                    <div id='signInDiv'></div>
-                </div>
-            </form>
+                
+                <div className = 'aboutApplication'>
+                    <div className = "bg-red-200 bg-opacity-20 backdrop-blur-sm rounded drop-shadow-lg px-5 pt-6 pb-8 mb-6">
+
+                        <p className = 'aboutText text-white' >
+
+                            Welcome to BAS Music Application! This application is a music streaming service that allows you to search for your favourite tracks, artists, and albums! You can also create your own playlists to hold all of your favourite music with full customization.
+
+                        </p>
+                        </div>
+                    
+                
+
+                <form className="bg-red-200 bg-opacity-20 backdrop-blur-sm rounded drop-shadow-lg px-8 pt-6 pb-8 mb-4">
+                    <div className="mb-4">
+                        <label className="block text-white text-sm font-bold mb-2" htmlFor="username">
+                            Username
+                        </label>
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
+                        </div>
+                    <div className="mb-6">
+                        <label className="block text-white text-sm font-bold mb-2" htmlFor="password">
+                            Password
+                        </label>
+                        <input className="shadow appearance-none rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
+                    </div>
+                    <div className="flex items-center justify-between">
+                        <button className="bg-black hover:bg-red-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={routeChange}>
+                            Sign In
+                        </button>
+                        <div id='signInDiv'></div>
+                    </div>
+                </form>
+            </div>
+            </div>
         </div>
-    </div>
+
+    </div> 
+    
   )
 }
+
 
 export default Login
