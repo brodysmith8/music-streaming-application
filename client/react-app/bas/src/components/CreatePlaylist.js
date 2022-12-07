79eafdb4d5c9aee8ec5c79f672f2db20135a40b7
saifattq <72181778+saifahmad02@users.noreply.github.com>
2022-12-07 00:51:23 -0500

    added create playlist + setting values func.

--- client/react-app/bas/src/components/CreatePlaylist.js
+++ client/react-app/bas/src/components/CreatePlaylist.js
@@ -1,16 +1,38 @@
 import React from 'react'
+import { useState } from 'react'
+
 
 const CreatePlaylist = () => {
-  return (
+  
+    const [plName, setPlName] = useState('');
+    const [plDesc, setPlDesc] = useState('');
+
+    const [checked, setChecked] = useState(false);
+
+    const [trackList, setTrackList] = useState('');
+    const [formattedList, setFormmatedList] = ([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(plName);
+        console.log(plDesc);
+        console.log(checked);
+
+
+    }
+
+
+    return (
     <div className='my-4 p-5 bg-[#330404] rounded-xl'>
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="mb-6">
                 <label htmlFor="listName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Playlist Name</label>
-                <input type="text" id="listName" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-[#330404] dark:focus:ring-red-500 dark:focus:border-red-500" placeholder="Enter a name" required />
+                <input type="text" id="listName" onChange={e => setPlName(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-[#330404] dark:focus:ring-red-500 dark:focus:border-red-500" placeholder="Enter a name" required />
             </div>
             <div className="mb-6">
                 <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Playlist Description</label>
-                <input type="text" id="description" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-[#330404] dark:focus:ring-red-500 dark:focus:border-red-500" />
+                <input type="text" id="description" onChange={e => setPlDesc(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-[#330404] dark:focus:ring-red-500 dark:focus:border-red-500" />
             </div>
             <div className="mb-6">
                 <label htmlFor="tracklist" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tracklist</label>
@@ -18,7 +40,7 @@ const CreatePlaylist = () => {
             </div>
             <div className="flex items-start mb-6">
                 <div className="flex items-center h-5">
-                    <input id="visibility" type="checkbox" value="" className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-red-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-red-500 dark:ring-offset-gray-800" required />
+                    <input id="visibility" type="checkbox" value={checked} onChange={e => setChecked(e.target.checked)} className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-red-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-red-500 dark:ring-offset-gray-800" />
                 </div>
                 <label htmlFor="visibility" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Private</label>
             </div>
